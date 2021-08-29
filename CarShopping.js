27451258e0af73bbd15ffc2127b3909c8bd983f5
DavidAlvarez34 <89229184+DavidAlvarez34@users.noreply.github.com>
2021-08-29 13:59:45 -0500

    Acomodando el tamaño de las cards

--- CarShopping.js
+++ CarShopping.js
@@ -79,8 +79,8 @@ class ShoppingCart {
     const shoppingCartRow = document.createElement("div");
     //cambiar los elementos
     const shoppingCartContent = `
-   <div class="row shoppingCartItem">
-                    <div class="col-6">
+   <div class="col-3 shoppingCartItem">
+                    <div class="col-3">
                         <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
                             <img src='${itemImage}' class="shopping-cart-image" width="80px" height="80px">
                             <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}
